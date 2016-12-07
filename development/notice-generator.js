0416410c785d932601e88c1066463627688d0025
Kevin Serrano <kevgagser@gmail.com>
2016-12-07 14:54:06 -0800

    Create npm command. Enhance notice generation.

--- development/notice-generator.js
+++ development/notice-generator.js
@@ -4,8 +4,9 @@ var prompt = require('prompt')
 var open = require('open')
 
 var id = 0
+var date = new Date().toDateString()
 
-fs.readdir('../notices', (err, files) => {
+fs.readdir('notices', (err, files) => {
   files.forEach(file => {
     id++
   })
@@ -14,11 +15,11 @@ fs.readdir('../notices', (err, files) => {
 prompt.start()
 
 prompt.get(['title'], function (err, result) {
-  fs.writeFile(`../notices/notice_${id}.md`, "", function (err) {
+  fs.writeFile(`notices/notice_${id}.md`, `# ${result.title}\n### ${date}\n[Message Goes Here]\n`, function (err) {
     if (err) {
       return console.log(err)
     } else {
-      open(`../notices/notice_${id}.md`)
+      open(`notices/notice_${id}.md`)
     }
   })
 })
